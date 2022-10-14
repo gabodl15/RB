d67366354d9c4596b629d525d157cbb5a8381741
Gabriel Díaz <gabodl15@zoho.com>
2022-10-14 16:56:50 -0400

    Cambiando la vista, el frontend, y validando autenticacion en paramiko.

--- nodos/views.py
+++ nodos/views.py
@@ -1,21 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 from .models import Nodo, CompanyAntenna
 import paramiko, json
 
 # FUNCTIONS THAT ARE NOT A VIEW
-def ssh_connection(antenna):
+def ssh_connection(request, antenna):
     host = antenna.ip
     username = antenna.username
     password = antenna.password
 
-    antenna_connection = paramiko.client.SSHClient()
-    antenna_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    antenna_connection.connect(host, username=username, password=password)
-    _stdin, _stdout, _stderr = antenna_connection.exec_command('wstalist')
-    get_result = _stdout.read().decode()
-    antenna_connection.close()
+    try:
+        antenna_connection = paramiko.client.SSHClient()
+        antenna_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        antenna_connection.connect(host, username=username, password=password)
+        _stdin, _stdout, _stderr = antenna_connection.exec_command('wstalist')
+        get_result = _stdout.read().decode()
+        antenna_connection.close()
+        return get_result
 
-    return get_result
+    except paramiko.ssh_exception.AuthenticationException:
+        messages.error(request, 'USUARIO O CLAVE INCORRECTOS')
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
 
 # Create your views here.
 def index(request):
@@ -29,7 +37,10 @@ def show(request, id):
 
 def antenna(request, ip):
     antenna = CompanyAntenna.objects.filter(ip=ip).first()
-    get_station_info = ssh_connection(antenna)
+    get_station_info = ssh_connection(request, antenna)
+    if isinstance(get_station_info, HttpResponseRedirect):
+        return get_station_info
+
     details= json.loads(get_station_info)
     context = {
         'details': details,
