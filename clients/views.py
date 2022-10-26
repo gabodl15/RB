919fe4f4821d8c588f648f9af6ee0957e0e13bb5
Gabriel Díaz <gabodl15@zoho.com>
2022-10-26 17:00:46 -0400

    Trabajando con folium

--- clients/views.py
+++ clients/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Client
 from .forms import ClientForm
+import folium
 
 class ClientCreateView(CreateView):
     model = Client
@@ -10,10 +11,31 @@ class ClientCreateView(CreateView):
     template_name = 'clients/create.html'
     success_url = '/clients'
 
+    def post(self, request, *args, **kwargs):
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            client = form.save()
+            if client.coordinates != '':
+                print('Trae coordenadas')
+            client.save()
+        return redirect('clients.index')
+
 def index(request):
     clients = Client.objects.all()
     return render(request, 'clients/index.html', {'clients': clients})
 
 def show(request, id):
     client = Client.objects.get(id=id)
-    return render(request, 'clients/show.html', {'client': client})
+    coordinates = client.coordinates
+    if coordinates:
+        x = coordinates.split(',')
+        map = folium.Map(location=[float(x[0]), float(x[1])], zoom_start=16)
+        folium.Marker([float(x[0]), float(x[1])], tooltip='Clip aqui').add_to(map)
+        map_render = map.get_root().render()
+    else:
+        map_render = None
+    context = {
+        'client': client,
+        'map_render': map_render
+    }
+    return render(request, 'clients/show.html', context)
