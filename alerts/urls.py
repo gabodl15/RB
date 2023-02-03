78dce3ed924978cc23e4e352929bcca7ea6955c7
Gabriel Díaz <gabodl15@zoho.com>
2023-02-03 16:22:40 -0400

    Mostrando las alertas por tipo.

--- alerts/urls.py
+++ alerts/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='alerts.index'),
+    path('attending/<int:id>/', views.attending, name='alerts.attending'),
+    path('solved/<int:id>/', views.solved, name='alerts.solved'),
+    path('delete/<int:id>/', views.delete, name='alerts.delete')
 ]
--- alerts/views.py
+++ alerts/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Alert
 # Create your views here.
 
 def index(request):
-    alerts = Alert.objects.filter(status='WT').order_by('-created')[:20]
+    # alerts = Alert.objects.filter(status='WT').order_by('-created')[:20]
+    alerts = Alert.objects.values('color').distinct()
+    alerts_by_color = {}
+
+    for color in alerts:
+        alerts_by_color[color['color']] = Alert.objects.filter(color=color['color']).exclude(status='SL')
     context = {
-        'alerts': alerts
+        'alerts_by_color': alerts_by_color
     }
     return render(request, 'alerts/index.html', context)
+
+def attending(request, id):
+    user = request.user
+    alert = Alert.objects.get(id=id)
+    alert.status = 'AT'
+    alert.attended_by = user
+    alert.save()
+    return redirect('alerts.index')
+
+def solved(request, id):
+    alert = Alert.objects.get(id=id)
+    alert.status = 'SL'
+    alert.save()
+    return redirect('alerts.index')
+
+def delete(request, id):
+    alert = Alert.objects.get(id=id)
+    alert.delete()
+    messages.success(request, 'Alerta Eliminada')
+    return redirect('alerts.index')
