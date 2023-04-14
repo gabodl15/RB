2937c61dccc3de4236d9d1e70407f746f662fb44
Gabriel Díaz <gabodl15@zoho.com>
2023-04-14 01:18:35 -0400

    Agregando el templatetag

---
+++ clients/templatetags/check_plan.py
@@ -0,0 +1,15 @@
+from django import template
+from routers.views import Connection
+
+register = template.Library()
+@register.filter
+def check_plan(profile):
+    conn = Connection(profile.router)
+    if conn.active:
+        active = conn.name_query('/ppp/active', profile.name)
+        conn.disconnect()
+        if len(active):
+            if active[0]['address'] != profile.plan.name:
+                return f"<span class='red-text'>{profile.plan.name}</span>"
+    else:
+        return profile.plan.name
